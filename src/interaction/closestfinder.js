0474a1407ee4df913551e51728e37d7311060f19
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2021-12-05 22:07:36 +0100

    Handle profile synchronization for all projections
    Use a small hack to avoid reprojecting to EPSG:4326.

--- src/interaction/closestfinder.js
+++ src/interaction/closestfinder.js
@@ -1,4 +1,5 @@
-import {distance, closestOnSegment} from 'ol/coordinate.js';
+import {distance as fakeDistance, closestOnSegment} from 'ol/coordinate.js';
+import GeometryLayout from 'ol/geom/GeometryLayout.js';
 
 /**
  * @typedef {Object} ClosestPoint
@@ -22,9 +23,13 @@ import {distance, closestOnSegment} from 'ol/coordinate.js';
  * @return {ClosestPoint}
  */
 export function findClosestPointInLine(line, searched, previousLineLength, interpolate) {
-  let currentLineLength = 0; // Euclidian length (of the line :p)
+  let currentLineLength = 0; // from XYZM data
   const coordinatess = line.getCoordinates();
   let previous = coordinatess[0];
+  if (line.getLayout() !== GeometryLayout.XYZM) {
+    console.error('invalid', line);
+    throw new Error('findClosestPointInLine works only with XYZM lines');
+  }
 
   /** @type {ClosestPoint} */
   const best = {
@@ -35,17 +40,28 @@ export function findClosestPointInLine(line, searched, previousLineLength, inter
 
   for (let i = 0; i < coordinatess.length; ++i) {
     let coordinates = coordinatess[i];
-    const segmentLength = distance(coordinates, previous);
+    const segmentLength = coordinates[3] - previous[3];
+    console.assert(Number.isFinite(segmentLength) && segmentLength >= 0);
     currentLineLength += segmentLength;
     const originalCoordinates = coordinates;
 
     let distanceFromStart = currentLineLength;
-    if (interpolate && i > 0) {
+    if (interpolate && i > 0 && segmentLength > 0) {
       const newCoordinates = closestOnSegment(searched, [previous, coordinates]);
-      distanceFromStart = currentLineLength - distance(coordinates, newCoordinates);
+      // FakeDistance is accurate for local projections in meters (like EPSG:2056)
+      // but not for mercator, because the distances between coordinates on the map
+      // depends on the latitude.
+      // Since we compare coordinates close to each other (at same latitude), the ratio of
+      // distances can be considered accurate with mercator.
+      // So, to avoid reprojecting our coordinates and use great circle formulas,
+      // we compute euclidian distances and use their ratio to get the interpolated (accurate) distance.
+      const fakeD1 = fakeDistance(previous, newCoordinates);
+      const fakeD2 = fakeDistance(newCoordinates, coordinates);
+      const estimatedMDistanceToEnd = segmentLength * fakeD2 / (fakeD1 + fakeD2);
+      distanceFromStart = currentLineLength - estimatedMDistanceToEnd;
       coordinates = newCoordinates;
     }
-    const distanceFromSearched = distance(searched, coordinates);
+    const distanceFromSearched = fakeDistance(searched, coordinates);
     if (distanceFromSearched < best.distanceFromSearched) {
       best.distanceFromSearched = distanceFromSearched;
       best.coordinates = coordinates;
