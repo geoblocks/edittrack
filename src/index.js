7413666cf555bdcf670e38cad2c4289188230ed7
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2021-12-18 22:42:10 +0100

    Use explicit names for exports
    Except for TrackManager which is the main class of this package.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export {default as TrackManager} from './interaction/TrackManager.js';
-export {default as ExtractFromSegment} from './profiler/ExtractFromSegment.js';
-export {default as Fallback} from './profiler/Fallback.js';
+export {default as ExtractFromSegmentProfiler} from './profiler/ExtractFromSegment.js';
+export {default as FallbackProfiler} from './profiler/Fallback.js';
 export {default as SwisstopoProfiler} from './profiler/SwisstopoProfiler.js';
-export {default as GraphHopper} from './router/GraphHopper.js';
+export {default as GraphHopperRouter} from './router/GraphHopper.js';
