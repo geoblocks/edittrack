2c864d8b58e30d91043cb476e0409a8c29d339ae
Pedro Soares <pedro.soares@camptocamp.com>
2024-03-06 17:13:39 +0100

    equilizer fix

--- src/interaction/TrackUpdater.ts
+++ src/interaction/TrackUpdater.ts
@@ -76,16 +76,6 @@ export default class TrackUpdater {
         if (after) {
           profileUpdates.push(this.profiler.computeProfile(after));
         }
-        if (pointFrom) {
-          const {before, after} = this.trackData.getAdjacentSegments(pointFrom);
-          if (before) profileUpdates.push(this.profiler.computeProfile(before));
-          if (after) profileUpdates.push(this.profiler.computeProfile(after));
-        }
-        if (pointTo) {
-          const {before, after} = this.trackData.getAdjacentSegments(pointTo);
-          if (before) profileUpdates.push(this.profiler.computeProfile(before));
-          if (after) profileUpdates.push(this.profiler.computeProfile(after));
-        }
         await Promise.all(profileUpdates);
       });
     }
@@ -95,17 +85,17 @@ export default class TrackUpdater {
   equalizeCoordinates(controlPoint: Feature<Point>) {
     const {before, after} = this.trackData.getAdjacentSegments(controlPoint);
     if (before && after) {
-      const firstCoordinate = before.getGeometry().getLastCoordinate();
-      const lastCoordinate = after.getGeometry().getFirstCoordinate();
+      const firstCoordinateXY = before.getGeometry().getLastCoordinate().slice(0, 2);
+      const lastCoordinateXY = after.getGeometry().getFirstCoordinate().slice(0, 2);
 
-      if (!equals(firstCoordinate.slice(0, 2), lastCoordinate.slice(0, 2))) {
+      if (!equals(firstCoordinateXY, lastCoordinateXY)) {
         // both segments were snapped but the middle point results from the two routing was not exactly the same.
         const beforeCoordinates = before.getGeometry().getCoordinates();
-        beforeCoordinates[beforeCoordinates.length - 1] = lastCoordinate;
+        // only replace the XY part of the last coordinate
+        beforeCoordinates[beforeCoordinates.length - 1].splice(0, 2, ...lastCoordinateXY);
         before.getGeometry().setCoordinates(beforeCoordinates);
-        console.log('equalizeCoordinates', lastCoordinate);
-        this.profiler.computeProfile(before);
-        controlPoint.getGeometry().setCoordinates(lastCoordinate);
+
+        controlPoint.getGeometry().setCoordinates(lastCoordinateXY);
       }
     }
   }
