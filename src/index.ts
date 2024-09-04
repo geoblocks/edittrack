e3a0901386c108a341360116d4dfd76c93340ddc
Tanguy Rossel <tanguy.rossel@camptocamp.com>
2024-09-04 08:50:48 +0200

    fix: add Densifier exports to main index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ export {default as FallbackProfiler} from './profiler/Fallback';
 export {default as SwisstopoProfiler} from './profiler/SwisstopoProfiler';
 export type {Profiler} from './profiler/index';
 
+export {default as SnappedDensifier} from './densifier/SnappedDensifier';
+export {default as UnsnappedDensifier} from './densifier/UnsnappedDensifier';
+export type {Densifier} from './densifier/index';
+
 export {default as GraphHopperRouter} from './router/GraphHopperRouter';
 export {default as OSRMRouter} from './router/OSRMRouter';
 export type {Router} from './router/index';
